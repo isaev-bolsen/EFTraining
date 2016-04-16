9311670c173b7f6fab7e80ff49b2996295e7e53f
isaev-bolsen <al_mamistov@mail.ru>
2016-04-16 11:08:49 +0300

    GUID

--- DataAdapter/DataAdapter.cs
+++ DataAdapter/DataAdapter.cs
@@ -11,7 +11,14 @@ namespace DataAdapter
 
         public void AddNew(string From, DateTime Dep, string To, DateTime Arr)
         {
-            DB.Trains.Add(new Train() { StationFrom = From, Departure = Dep, StationTo = To, Arrival = Arr });
+            DB.Trains.Add(new Train()
+            {
+                StationFrom = From,
+                Departure = Dep,
+                StationTo = To,
+                Arrival = Arr,
+                ID = Guid.NewGuid()
+            });
             DB.SaveChanges();
         }
     }
